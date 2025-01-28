a124d35ba84830082f9574fb03c61f052a200b06
Arthur421459 <mightypupspjmaks@gmail.com>
2025-01-28 17:43:21 -0300

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function phone() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+     }
+    else {
+       return false;
+     }
+}
+
+isphone = phone()
+if (isphone) {
+    var bannerlogo = document.getElementById("bannerlogo")
+    bannerlogo.id = "bannerlogop"
+}
